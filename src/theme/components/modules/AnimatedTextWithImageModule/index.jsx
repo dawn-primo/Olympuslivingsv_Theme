47da362a57a281720345a1642dc9958702bc302f
dev-sp24 <savan.thoughtmate@gmail.com>
2025-06-13 11:33:01 +0530

    Change Code

--- src/theme/components/modules/AnimatedTextWithImageModule/index.jsx
+++ src/theme/components/modules/AnimatedTextWithImageModule/index.jsx
@@ -26,7 +26,7 @@ export const Component = props => {
             <Island
               module={AnimatedText}
               hydrateOn="idle"
-              props={{ add_animated_text }}
+              add_animated_text={{ add_animated_text }}
               clientOnly
             />
 
