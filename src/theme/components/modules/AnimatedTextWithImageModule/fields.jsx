bd2dd88f480acff984d435a4bd3a9cdc8510fd34
dev-sp24 <savan.thoughtmate@gmail.com>
2025-06-13 12:00:05 +0530

    Add Heading

--- src/theme/components/modules/AnimatedTextWithImageModule/fields.jsx
+++ src/theme/components/modules/AnimatedTextWithImageModule/fields.jsx
@@ -6,32 +6,33 @@ import {
   RepeatedFieldGroup
 } from '@hubspot/cms-components/fields';
 import CommonStylesSpacingFields from '../../shared/SpacingStyleComponent/CommonStylesSpacingFields.jsx';
+import HeadingContent from '../../shared/HeadingComponent/HeadingContent.jsx';
 
 
 export const fields = (
   <ModuleFields>
+    <HeadingContent headingTextLabel='Heading text' />
+    <TextField name='animation_prefix_text' label='Animation Prefix Text'></TextField>
+    <TextField name='animation_suffix_text' label='Animation Suffix Text'></TextField>
+    <RepeatedFieldGroup name='add_animated_text' label='Add Animated Text'>
+      <TextField name='animated_text' label='Animated Text'></TextField>
+    </RepeatedFieldGroup>
 
-      <TextField name='animation_prefix_text' label='Animation Prefix Text'></TextField>
-      <TextField name='animation_suffix_text' label='Animation Suffix Text'></TextField>
-      <RepeatedFieldGroup name='add_animated_text' label='Add Animated Text'>
-         <TextField name='animated_text' label='Animated Text'></TextField>
-      </RepeatedFieldGroup>
-    
-     <ImageField
-            label="Image"
-            name="image"
-            resizable={true}
-            responsive={true}
-            showLoading={true}
-          >
-          </ImageField>
-   
+    <ImageField
+      label="Image"
+      name="image"
+      resizable={true}
+      responsive={true}
+      showLoading={true}
+    >
+    </ImageField>
+
     <FieldGroup
       label='Styles'
       name='groupStyle'
       tab='STYLE'
     >
-
+      <HeadingStyles headingStyleAsDefault="h2" />
       <CommonStylesSpacingFields />
 
     </FieldGroup>
